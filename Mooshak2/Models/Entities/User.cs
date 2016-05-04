fd1bfe570641cbeebbfbf29e9a79a9b9f5ec7e2b
Gulla <gulla_d@hotmail.com>
2016-05-04 15:40:12 +0000

    Yo komið afangar inn i courses töflu :*

---
+++ Mooshak2/Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+    }
+}
